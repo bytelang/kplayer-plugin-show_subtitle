f62ad92c561849b0ffdc8bf338ec4527ea5d7024
karaler <karaler@bytelang.com>
2021-01-31 19:36:31 +0800

    feat: 添加ass字幕文件的支持

--- KPVideoShowSubtitlePlugin.cpp
+++ KPVideoShowSubtitlePlugin.cpp
@@ -15,10 +15,23 @@ KPVideoShowSubtitlePlugin::KPVideoShowSubtitlePlugin(const std::string &identify
     KPlayer::FileInfo file_info(current_title);
 
     // 字幕文件路径
+    std::string subtitle_file_path;
+
     std::string       srt_file_path = file_info.GetBaseFilePath() + file_info.GetBaseFileName() + ".srt";
     KPlayer::FileInfo srt_file_info(srt_file_path);
-    if (!srt_file_info.Exists()) {
-        throw KPFilterException("字幕文件加载失败；文件不存在 path: " + srt_file_path);
+
+    std::string       ass_file_path = file_info.GetBaseFilePath() + file_info.GetBaseFileName() + ".ass";
+    KPlayer::FileInfo ass_file_info(ass_file_path);
+
+    if (srt_file_info.Exists()) {
+        subtitle_file_path = srt_file_path;
+    }
+    if (ass_file_info.Exists()) {
+        subtitle_file_path = ass_file_path;
+    }
+
+    if (subtitle_file_path.empty()) {
+        throw KPFilterException("字幕文件加载失败；ass与srt同名字幕文件不存在 path: " + file_info.GetBaseFilePath() + file_info.GetBaseFileName());
     }
 
     // 赋值described
